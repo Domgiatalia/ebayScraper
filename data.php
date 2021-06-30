0be30c40f74dad3f9005a563fccb6a1f939002bf
Domgiatalia <72265261+Domgiatalia@users.noreply.github.com>
2021-06-30 10:59:31 +0100

    php to python

--- data.php
+++ data.php
@@ -2,4 +2,15 @@
 set_time_limit(0);
 $action = $_POST['action'];
 
+$commandStart = 'https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=';
+$commandMid = '';
+$commandEnd = '&_sacat=0&_ipg=25';
+$commandMid = str_replace(' ', '+', $action);
+$scr = $commandStart . $commandMid . $commandEnd;
+
+$command = escapeshellcmd("C:\Users\domzt\AppData\Local\Programs\Python\Python39\python.exe script.py $scr");
+$output = shell_exec($command);
+
+
+
 ?>
