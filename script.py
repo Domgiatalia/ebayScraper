94b94adeb06963746c45774762d97ebe5be7138f
Domgiatalia <72265261+Domgiatalia@users.noreply.github.com>
2021-06-30 11:01:07 +0100

    python test

---
+++ script.py
@@ -0,0 +1,14 @@
+# Import libraries
+import requests
+import urllib.request
+import time
+import csv
+import json
+import requests
+import sys
+
+from bs4 import BeautifulSoup
+
+url = sys.argv[1]
+testlist = []
+
